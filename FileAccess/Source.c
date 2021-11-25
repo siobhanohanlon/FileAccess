2daaea8416ebd9e22806b64ad8c48253cfb00f0f
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-11-25 15:37:30 +0000

    Load File

--- FileAccess/Source.c
+++ FileAccess/Source.c
@@ -9,14 +9,71 @@ void main()
 	//Declare Variables
 	char name[30];
 	char surname[30];
-	int age, sales, customerID, noCustomers, counter;
+	int age, sales, customerID, noCustomers, counter, numInputs;
 	int combinedSales = 0, maxAge = 0, minAge = 150, maxSales = 0; //So anything thats added is smaller than 150 and over 0
 	
 	//Load info from existing files
+	//Customer File
+	//Read
+	customerFile = fopen("Customers.txt", "r");
+
+	//Check if File is Open
+	if (customerFile == NULL)
+	{
+		printf("Sorry the Customer File could not be opened!!!\n");
+
+		//Exit
+		exit(0);
+	}
+
+	else
+	{
+		//Read to end of file
+		while (!feof(customerFile))
+		{
+			//Count how many Lines
+			numInputs = fscanf(customerFile, "%s %s %d %d %d", name, surname, &sales, &age, &customerID);
+
+			//Display according to numInputs
+			if (numInputs == 5)
+			{
+				printf("Customer:%s %s Sales:%d Age:%d ID:%d\n", name, surname, sales, age, customerID);
+			}
+		}
+
+		//Close file
+		fclose(customerFile);
+	}
+
+	//SalesFile
+	//Customer File
+	//Read
+	salesFile = fopen("Sales.txt", "r");
+
+	//Check if File is Open
+	if (salesFile == NULL)
+	{
+		printf("Sorry the Sales File could not be opened!!!\n");
+
+		//Exit
+		exit(0);
+	}
+
+	else
+	{
+		//Count how many Lines
+		fscanf(salesFile, "%d %d %d %d", &combinedSales, &maxSales, &maxAge, &minAge);
+
+		//Close file
+		fclose(salesFile);
+
+		//Display according to numInputs
+		printf("Inital Total Sales: %d Max Sales: %d Oldest: %d Youngest: %d\n", combinedSales, maxSales, maxAge, minAge);
+	}
 
 
 	//Ask how many customers
-	printf("Please enter the number of customers that you're entering:\t");
+	printf("\nPlease enter the number of customers that you're entering:\t");
 	scanf("%d", &noCustomers);
 
 	//Open Customer File to append
@@ -30,7 +87,7 @@ void main()
 		scanf("%s %s %d %d %d", name, surname, &sales, &age, &customerID);
 
 		//Display
-		printf("%s %s %d %d %d", name, surname, sales, age, customerID);
+		printf("Customer:%s %s Sales:%d Age:%d ID:%d", name, surname, sales, age, customerID);
 
 		//If File is opened, print
 		if (customerFile != NULL)
@@ -58,7 +115,7 @@ void main()
 	}//End Loop for getting customer details
 
 	//Display 
-	printf("\n\n\n%d %d %d %d", combinedSales, maxSales, maxAge, minAge);
+	printf("\n\n\nTotal Sales:%d Max Sales:%d Oldest:%d Youngest:%d", combinedSales, maxSales, maxAge, minAge);
 
 	//If File is Open
 	if (customerFile != NULL)
@@ -84,4 +141,7 @@ void main()
 		//Close File
 		fclose(salesFile);
 	}
+
+	//Skip Lines
+	printf("\n\n");
 }
