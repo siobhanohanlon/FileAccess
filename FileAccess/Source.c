7b285309ba2862eb1b75c84a6db42a8a6675b9c5
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-11-25 14:38:29 +0000

    declare variables

--- FileAccess/Source.c
+++ FileAccess/Source.c
@@ -2,5 +2,16 @@
 
 void main()
 {
+	//Declare File Names
+	FILE* customerFile;
+	FILE* salesFile;
+
+	//Declare Variables
+	char name[30];
+	char surname[30];
+	int age, sales, customerNo, noCustomers, counter;
+	int combinedSales = 0, maxAges = 0, minAge = 150; //So anything thats added is smaller than 150 and over 0
+	
+	//Load info from existing files
 
 }
